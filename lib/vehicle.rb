4227b13b2f1422dd745e5f150af404e850879724
Xiaoyi Liu <lxy878@gmail.com>
2020-03-12 23:03:15 +0000

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,15 @@
 class Vehicle
+  attr_accessor :wheel_size, :wheel_number
+  def initialize(wheel, number)
+    @wheel_size = wheel
+    @wheel_number = number
+  end
 
+  def go
+    'vrrrrrrrooom!'
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
 end
